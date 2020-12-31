c3462b816deeec7c26572677a0ebab4dae5d84ee
Hkc-1673 <habibkc71@gmail.com>
2020-12-31 19:08:56 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lets build gmail clone</h1>
+    </div>
+  );
+}
+
+export default App;
