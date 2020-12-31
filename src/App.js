681d36dc4856bcc946735a7558dea10a32205dd6
Hkc-1673 <habibkc71@gmail.com>
2020-12-31 19:47:38 +0200

    Create component Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Header/>
       <h1>lets build gmail clone</h1>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+import MenuIcon from '@material-ui/icons/Menu';
+
+function Header() {
+    return (
+        <div className="header">
+        <div className="header__left"></div>
+        </div>
+    )
+}
+
+export default Header;
