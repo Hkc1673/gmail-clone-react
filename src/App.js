dd7648d7b531b0d5c0c9e07e540f3bd466e407fb
Hkc-1673 <habibkc71@gmail.com>
2021-01-01 17:22:17 +0200

    Completed sidebar design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header";
-
+import Sidebar from "./components/Sidebar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Sidebar />
     
     </div>
   );
