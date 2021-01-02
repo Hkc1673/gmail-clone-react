cf888cad1058602753dc0452f2c8b615641d5b61
Hkc-1673 <habibkc71@gmail.com>
2021-01-02 20:39:29 +0200

    Connect database

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,11 +1,25 @@
 import { Checkbox, IconButton } from '@material-ui/core'
 import { ArrowDropDown, ChevronLeft, ChevronRight, Inbox, KeyboardHide, LocalOffer, MoreVert, People, Redo, Settings } from '@material-ui/icons'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Section from "./Section"
 import "./EmailList.css"
 import EmailRow from "./EmailRow"
+import { db } from "../firebase"
 
 function EmailList() {
+    const [emails, setEmails] = useState([]);
+
+    useEffect(() => {
+        db.collection("emails").orderBy("timestamp", "desc").onSnapshot
+            ((snapshot) => setEmails(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data(),
+            }))
+            )
+            );
+    }, [])
+    console.log(emails)
+
     return (
         <div className="emailList">
             <div className="emailList__settings">
@@ -40,22 +54,22 @@ function EmailList() {
             </div>
             <div className="emailList_sections">
                 <Section Icon={Inbox} title="Primary" color="red" selected />
-                <Section Icon={People} title="Social" color="#1A73E8"  />
-                <Section Icon={LocalOffer} title="Promotions" color="green"  />
+                <Section Icon={People} title="Social" color="#1A73E8" />
+                <Section Icon={LocalOffer} title="Promotions" color="green" />
             </div>
             <div className="emailList__list">
-                <EmailRow 
-                    title="Twitch"
-                    subject="Hey fellow streamer!!"
-                    description="This is a test"
-                    time="10pm"
-                />
-                <EmailRow 
-                    title="Twitch"
-                    subject="Hey fellow streamer!!"
-                    description="This is a testThis is a testThis is a testThis is This is a testThis is a testa testThis is a test"
-                    time="10pm"
-                />
+                {
+                    emails.map(({ id, data: { to, subject, message, timestamp } }) => (
+                        <EmailRow
+                            id={id}
+                            key={id}
+                            title={to}
+                            subject={subject}
+                            description={message}
+                            time={new Date(timestamp?.seconds * 1000).toUTCString()}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
--- src/components/SendMail.js
+++ src/components/SendMail.js
@@ -5,13 +5,24 @@ import "./SendMail.css"
 import { useForm } from "react-hook-form";
 import { useDispatch } from 'react-redux';
 import { closeSendMessage } from '../features/mailSlice';
+import { db } from "../firebase";
+import firebase from "firebase";
 
 function SendMail() {
     const { register, handleSubmit, watch, errors } = useForm();
     const dispatch = useDispatch();
 
     const onSubmit = (formData) => {
-        console.log(formData)
+        console.log(formData);
+        db.collection("emails").add(
+            {
+                to: formData.to,
+                subject: formData.subject,
+                message: formData.message,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+
+        dispatch(closeSendMessage())
     }
 
     return (
